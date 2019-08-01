9187bbe804b98f5e6c8a7801a596a19a54bfd9c4
Fabricia Diniz <afabriciadiniz@gmail.com>
2019-08-01 14:30:14 -0300

    aula 2

--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -2,21 +2,45 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.Servidor;
+import br.com.alura.loja.modelo.Carrinho;
 
 public class ClienteTest {
 
+	private HttpServer server;
+	
+	@Before
+	public void startaServidor() {
+		this.server = Servidor.inicializaServidor();
+	}
+	
+	@After
+	public void mataServidor() {
+		server.stop();
+	}
+	
     @Test
-    public void testaQueAConexaoComOServidorFunciona() {
+    public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
     	
     	Client client = ClientBuilder.newClient();
+    	
     	// pega a URI base do servidor para fazer as requisições
-    	WebTarget target = client.target("http://www.mocky.io");
+    	WebTarget target = client.target("http://localhost:8000");
+    	
     	// faz uma requisição get para o path, parseando para String
-    	String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
+    	String conteudo = target.path("/carrinhos").request().get(String.class);
     	
     	// checa se o conteudo retornado é o que se estava esperando e a conexão com o servidor está ok
-    	Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+    	Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+    	Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+    
     }
 }
--- src/test/java/ProjetoTest.java
+++ src/test/java/ProjetoTest.java
@@ -2,19 +2,40 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.Servidor;
+import br.com.alura.loja.modelo.Projeto;
 
 public class ProjetoTest {
 	
+	private HttpServer server;
+	
+	@Before
+	public void startaServidor() {
+		this.server = Servidor.inicializaServidor();
+	}
+	
+	@After
+	public void mataServidor() {
+		server.stop();
+	}
+	
 	@Test
-	public void testaQueAConexaoComOServidorFunciona() {
+	public void testaQueBuscarUmProjetoTrazOProjetoEsperado() {
 		
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/");
+		WebTarget target = client.target("http://localhost:8000/");
 		String conteudo = target.path("/projetos").request().get(String.class);
 		
-		Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Minha loja", projeto.getNome());
 	}
 
 }
